f1b11764a5834a1d01608015abf465cf92ff79fd
LcpMarvel <lcpmarvel@gmail.com>
2023-02-08 19:57:53 +0800

    replace trojan installer

--- src/templates/installer.sh
+++ src/templates/installer.sh
@@ -1,25 +1,10 @@
-cp /etc/apt/sources.list /etc/apt/sources.list.bakup
+sudo bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 
-cat << EOF >> /etc/apt/sources.list
-deb http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic main restricted universe multiverse
-deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic main restricted universe multiverse
-deb http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-security main restricted universe multiverse
-deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-security main restricted universe multiverse
-deb http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-updates main restricted universe multiverse
-deb http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-proposed main restricted universe multiverse
-deb http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ bionic-backports main restricted universe multiverse
-EOF
+CONFIGPATH=/usr/local/etc/trojan
 
-apt-get update
-
-apt-get install trojan -y
-
-mv /etc/trojan/config.json /etc/trojan/config.json.bk
-touch /etc/trojan/config.json
-cat << EOF >> /etc/trojan/config.json
+mv ${CONFIGPATH}/config.json ${CONFIGPATH}/config.json.bk
+touch ${CONFIGPATH}/config.json
+cat << EOF >> ${CONFIGPATH}/config.json
 {{ trojanConfig }}
 EOF
 
@@ -29,7 +14,7 @@ adduser trojan sudo
 mkdir /home/trojan/ssl
 mv /root/{{ sslKeyName }} /home/trojan/ssl/
 mv /root/{{ sslCertName }} /home/trojan/ssl/
-chown trojan:trojan /etc/trojan/config.json
+chown trojan:trojan ${CONFIGPATH}/config.json
 chown -R trojan:trojan /home/trojan/ssl
 
 systemctl enable trojan
